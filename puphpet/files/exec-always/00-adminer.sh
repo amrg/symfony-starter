ac0eb0359c6ff39c08846571ceee7187c0bfce62
Antonio Miguel Rodriguez Gallardo <loki.catoblepas@gmail.com>
2016-03-02 15:38:57 +0100

    vagrant box updated

--- puphpet/files/exec-always/00-adminer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f /var/www/html/*
-wget https://www.adminer.org/static/download/4.2.2/adminer-4.2.2-mysql-en.php -O /var/www/html/index.php
-wget https://raw.githubusercontent.com/vrana/adminer/master/designs/pappu687/adminer.css -O /var/www/html/adminer.css
--- puphpet/files/startup-always/01-composer-update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-php -n -d xdebug.remote_autostart=0 -d xdebug.remote_enable=0 -d xdebug.profiler_enable=0 /usr/local/bin/composer self-update
